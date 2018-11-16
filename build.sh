475a733087546bdd3bd0de326188c79e56329ce2
KoharaKazuya <reflect.results@gmail.com>
2018-11-16 21:45:17 +0900

    rich scripts

--- build.sh
+++ build.sh
@@ -4,7 +4,22 @@ set -eu
 
 cd "$(dirname "$0")"
 
+reset="$(tput sgr0    2>/dev/null || : )"
+rev="$(  tput rev     2>/dev/null || : )"
+white="$(tput setaf 7 2>/dev/null || : )"
+green="$(tput setaf 2 2>/dev/null || : )"
+
+echo ''
+echo "$white$rev INFO $reset Build theme"
+echo ''
 (cd ./themes/lightning; yarn install && yarn build)
 
+echo ''
+echo "$white$rev INFO $reset Build content"
+echo ''
 rm -rf public/
 hugo --minify
+
+echo ''
+echo "$green$rev  OK  $reset$green Done!"
+echo ''
